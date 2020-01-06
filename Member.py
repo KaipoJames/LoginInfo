c58bf30c3fd685745a4843c0f927398fa17b3a55
KaipoJames <59562158+KaipoJames@users.noreply.github.com>
2020-01-06 01:15:37 -1000

    Member.py
    A simple member class with different attributes

---
+++ Member.py
@@ -0,0 +1,11 @@
+
+class Member:
+
+    def __init__(self, name, member_id, age, telephone, email, username, password):
+        self.name = name
+        self.member_id = member_id
+        self.age = age
+        self.telephone = telephone
+        self.email = email
+        self.username = username
+        self.password = password
